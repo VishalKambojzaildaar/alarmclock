c53f9716250de593efd70f475e11e88a1352c652
VishalKambojzaildaar <143921566+VishalKambojzaildaar@users.noreply.github.com>
2023-10-05 23:26:00 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,145 +1,58 @@
-const currentTime = document.querySelector("#current-time");
-const setHours = document.querySelector("#hours");
-const setMinutes = document.querySelector("#minutes");
-const setSeconds = document.querySelector("#seconds");
-const setAmPm = document.querySelector("#am-pm");
-const setAlarmButton = document.querySelector("#submitButton");
-const alarmContainer = document.querySelector("#alarms-container");
+const timer = document.getElementById("stopwatch");
+var hr = 0;
+var min = 0;
+var sec = 0;
+var stoptime = true;
 
-// Adding Hours, Minutes, Seconds in DropDown Menu
-window.addEventListener("DOMContentLoaded", (event) => {
-
-  dropDownMenu(1, 12, setHours);
-
-  dropDownMenu(0, 59, setMinutes);
-
-  dropDownMenu(0, 59, setSeconds);
-
-  setInterval(getCurrentTime, 1000);
-  fetchAlarm();
-});
-
-// Event Listener added to Set Alarm Button
-setAlarmButton.addEventListener("click", getInput);
-
-
-function dropDownMenu(start, end, element) {
-  for (let i = start; i <= end; i++) {
-    const dropDown = document.createElement("option");
-    dropDown.value = i < 10 ? "0" + i : i;
-    dropDown.innerHTML = i < 10 ? "0" + i : i;
-    element.appendChild(dropDown);
+function startTimer() {
+  if (stoptime == true) {
+    stoptime = false;
+    timerCycle();
+  }
+}
+function stopTimer() {
+  if (stoptime == false) {
+    stoptime = true;
   }
 }
 
+function timerCycle() {
+  if (stoptime == false) {
+    sec = parseInt(sec);
+    min = parseInt(min);
+    hr = parseInt(hr);
 
-function getCurrentTime() {
-  let time = new Date();
-  time = time.toLocaleTimeString("en-US", {
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-    hour12: true,
-  });
-  currentTime.innerHTML = time;
+    sec = sec + 1;
 
-  return time;
-}
-
-
-function getInput(e) {
-  e.preventDefault();
-  const hourValue = setHours.value;
-  const minuteValue = setMinutes.value;
-  const secondValue = setSeconds.value;
-  const amPmValue = setAmPm.value;
-
-  const alarmTime = convertToTime(
-    hourValue,
-    minuteValue,
-    secondValue,
-    amPmValue
-  );
-  setAlarm(alarmTime);
-}
-
-// Converting time to 24 hour format
-function convertToTime(hour, minute, second, amPm) {
-  return `${parseInt(hour)}:${minute}:${second} ${amPm}`;
-}
-
-
-function setAlarm(time, fetching = false) {
-  const alarm = setInterval(() => {
-    if (time === getCurrentTime()) {
-      alert("Alarm Ringing");
+    if (sec == 60) {
+      min = min + 1;
+      sec = 0;
     }
-    console.log("running");
-  }, 500);
+    if (min == 60) {
+      hr = hr + 1;
+      min = 0;
+      sec = 0;
+    }
 
-  addAlaramToDom(time, alarm);
-  if (!fetching) {
-    saveAlarm(time);
+    if (sec < 10 ) {
+      sec = "0" + sec;
+    }
+    if (min < 10 ) {
+      min = "0" + min;
+    }
+    if (hr < 10 ) {
+      hr = "0" + hr;
+    }
+
+    timer.innerHTML = hr + ":" + min + ":" + sec;
+
+    setTimeout("timerCycle()", 1000);
   }
 }
-
-// Alarms set by user Dislayed in HTML
-function addAlaramToDom(time, intervalId) {
-  const alarm = document.createElement("div");
-  alarm.classList.add("alarm", "mb", "d-flex");
-  alarm.innerHTML = `
-              <div class="time">${time}</div>
-              <button class="btn delete-alarm" data-id=${intervalId}>Delete</button>
-              `;
-  const deleteButton = alarm.querySelector(".delete-alarm");
-  deleteButton.addEventListener("click", (e) => deleteAlarm(e, time, intervalId));
-
-  alarmContainer.prepend(alarm);
-}
-
-// Is alarms saved in Local Storage?
-function checkAlarams() {
-  let alarms = [];
-  const isPresent = localStorage.getItem("alarms");
-  if (isPresent) alarms = JSON.parse(isPresent);
-
-  return alarms;
-}
-
-// save alarm to local storage
-function saveAlarm(time) {
-  const alarms = checkAlarams();
-
-  alarms.push(time);
-  localStorage.setItem("alarms", JSON.stringify(alarms));
-}
-
-// Fetching alarms from local storage
-function fetchAlarm() {
-  const alarms = checkAlarams();
-
-  alarms.forEach((time) => {
-    setAlarm(time, true);
-  });
-}
-
-
-function deleteAlarm(event, time, intervalId) {
-  const self = event.target;
-
-  clearInterval(intervalId);
-
-  const alarm = self.parentElement;
-  console.log(time);
-
-  deleteAlarmFromLocal(time);
-  alarm.remove();
-}
-
-function deleteAlarmFromLocal(time) {
-  const alarms = checkAlarams();
-
-  const index = alarms.indexOf(time);
-  alarms.splice(index, 1);
-  localStorage.setItem("alarms", JSON.stringify(alarms));
+function resetTimer() {
+  timer.innerHTML = "00:00:00";
+  stoptime = true;
+  hr = 0;
+  sec = 0;
+  min = 0;
 }
